d18f47ba008d01d3f2c855efa99d3347530cf4f0
Neil Kandalgaonkar <neilk@brevity.org>
2017-07-05 17:45:50 -0700

    git logs as JSON

---
+++ git-log2json.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#  https://gist.github.com/textarcana/1306223
+# Use this one-liner to produce a JSON literal from the Git log:
+
+git log \
+    --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},' \
+    $@ | \
+    perl -pe 'BEGIN{print "["}; END{print "]\n"}' | \
+    perl -pe 's/},]/}]/'
+
